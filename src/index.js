38de4ab32392ff40c45ab23f15580d62eb8cfd93
Pedro Pablo <pedrosanchezcief@gmail.com>
2025-08-25 22:45:24 +0200

    new commit js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 const fn = () => {
-    // const miString = "soy un string"
-    // let b = miString + "muy feliz"
-    // console.log(b)
+    const miString = "soy un string"
+    let b = miString + "muy feliz"
+    console.log(b)
+    console.log('hola mundo');
+    
+    
 }
+
+fn()
